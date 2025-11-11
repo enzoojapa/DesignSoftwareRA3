f4926800ffc95385eba066b8f287602fac62f990
Vinicius Soares <vinicius.soaressilva06@gmail.com>
2025-11-11 19:43:37 -0300

    chore: update dependencies and move persistence.xml location

--- CRUD_java/src/main/java/module-info.java
+++ CRUD_java/src/main/java/module-info.java
@@ -2,5 +2,10 @@ module br.pucpr.crud_java {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+    requires jakarta.persistence;
+
+    opens br.pucpr.crud_java to javafx.fxml;
+
     exports br.pucpr.crud_java;
 }
